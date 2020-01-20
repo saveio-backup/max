45144b2604669ac8fd114b68e0a0ff12d7f0afb8
fuwei1217 <fuwei@onchain.com>
2020-01-20 19:19:25 +0800

    update timeout for wait block confirmation to make sure block is generated

--- max/max.go
+++ max/max.go
@@ -66,8 +66,8 @@ const (
 )
 
 const (
-	MAX_RETRY_REQUEST_TIMES                = 6  // max request retry times
-	MAX_REQUEST_TIMEWAIT                   = 1  // request time wait in second
+	MAX_RETRY_REQUEST_TIMES                = 3  // max request retry times
+	MAX_REQUEST_TIMEWAIT                   = 5  // request time wait in second
 	PROVE_FILE_INTERVAL                    = 10 // 10s proves
 	MAX_PROVE_FILE_ROUTINES                = 10 // maximum of concurrent check prove files
 	DEFAULT_REMOVE_NOTIFY_CHANNEL_SIZE     = 10 // default remove notify channel size
