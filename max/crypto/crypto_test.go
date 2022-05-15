34bc023021a310ec83d547094a7ca3572dab3a29
smallyunet <dev.wangyu@gmail.com>
2022-05-16 02:25:04 +0800

    add ecies length test

--- max/crypto/crypto_test.go
+++ max/crypto/crypto_test.go
@@ -1,7 +1,10 @@
 package crypto
 
 import (
+	"fmt"
 	"github.com/pborman/uuid"
+	"github.com/saveio/themis/crypto/encrypt"
+	"github.com/saveio/themis/crypto/keypair"
 	"testing"
 )
 
@@ -23,10 +26,11 @@ func TestAESDecryptFile(t *testing.T) {
 
 func TestCEIESEncryptFile(t *testing.T) {
 	file := "/Users/smallyu/work/test/file/aaa"
-	err := CEIESEncryptFile(file, "pwd", file+".ceies")
+	s, err := CEIESEncryptFile(file, "pwd", file+".ceies", nil)
 	if err != nil {
 		t.Error(err)
 	}
+	t.Log(s)
 }
 
 func TestCEIESDecryptFile(t *testing.T) {
@@ -40,4 +44,36 @@ func TestCEIESDecryptFile(t *testing.T) {
 func TestUUID(t *testing.T) {
 	u := uuid.New()
 	t.Log(u)
+}
+
+func TestEciesLength(t *testing.T) {
+	_, pub2, err := keypair.GenerateKeyPair(keypair.PK_ECIES, keypair.P256)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+
+	message := []byte("Hello, world.")
+	ct, err := encrypt.Encrypt(encrypt.AES128withSHA256, pub2, message, nil, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+	t.Log(len(ct))
+
+	message2 := []byte("Hello, world. Hello, world.")
+	ct2, err := encrypt.Encrypt(encrypt.AES128withSHA256, pub2, message2, nil, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+	t.Log(len(ct2))
+
+	message3 := []byte("Hello, world. Hello, world. Hello, world.")
+	ct3, err := encrypt.Encrypt(encrypt.AES128withSHA256, pub2, message3, nil, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+	t.Log(len(ct3))
 }
